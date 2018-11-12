7a383897078d70ed98277a1fa63ce5260de031c7
infernalslam <infernal-slam@hotmail.com>
2018-11-13 01:28:16 +0700

    feature: console.log()

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,6 +11,9 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
+  mounted () {
+    console.log('Welcome to LIFF Debug 🤪')
+  },
   components: {
     HelloWorld
   }
